cb7dd465082d82d4f6bd1c2654c266502d6b44b2
guillermotoza <guillrmotoza@gmail.com>
2024-11-25 18:48:05 -0600

    creacion de app clientes

--- sistemardp/clientes/models.py
+++ sistemardp/clientes/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
 class Cliente(models.Model):
-    nombre = models.CharField(max_length=255)
+    
+    nombre = models.CharField(max_length=100)
     correo = models.EmailField(unique=True)
     direccion = models.TextField()
-    detalles_membresia = models.TextField(blank=True, null=True)
-    fecha_registro = models.DateTimeField(auto_now_add=True)
-    fecha_baja = models.DateTimeField(null=True, blank=True)
-    historial_rentas = models.TextField(blank=True, null=True)
+    detalles_membresia = models.CharField(max_length=100)
+
+   
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_actualizacion = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.nombre
