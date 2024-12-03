64ac92338ac65bb004ad1da07076fed9d0a65b62
moisesr99 <45242281+moisesr99@users.noreply.github.com>
2024-12-03 11:57:08 -0600

    busqueda funcionando

--- sistemardp/peliculas/views.py
+++ sistemardp/peliculas/views.py
@@ -13,18 +13,18 @@ def busqueda_peliculas(request):
 
 def resultados_peliculas(request):
     if request.GET.get("pelicula") and request.GET.get("fecha_inicio") and request.GET.get("fecha_devolver"):
-        pelicula = request.GET.get("pelicula")
+        pelicula_nombre = request.GET.get("pelicula")
         fecha_inicio_str = request.GET.get("fecha_inicio")
         fecha_devolver_str = request.GET.get("fecha_devolver")
 
         fecha_inicio = datetime.strptime(fecha_inicio_str, '%Y-%m-%d').date() if fecha_inicio_str else None
         fecha_devolver = datetime.strptime(fecha_devolver_str, '%Y-%m-%d').date() if fecha_devolver_str else None
 
-        if len(pelicula) > 25 :
+        if len(pelicula_nombre) > 25 :
             messages.error(request, 'texto de busqueda demasiado largo')
             return redirect('busqueda_peliculas')
         
-        elif pelicula == "venom" :
+        elif pelicula_nombre == "venom" :
             messages.error(request, 'que fea pelicula, elije otra')
             return redirect('busqueda_peliculas')
 
@@ -36,7 +36,7 @@ def resultados_peliculas(request):
             messages.error(request, "la fecha de inicio de renta no puede ser anterior al dia actual")
 
         else:
-            pelicula_buscar = pelicula.objects.filter(titulo__icontains=pelicula)
+            pelicula_buscar = pelicula.objects.filter(titulo__icontains=pelicula_nombre)
             return render(request, "resultados_busqueda_peliculas.html",{"peliculas":pelicula_buscar })
 
     else:
