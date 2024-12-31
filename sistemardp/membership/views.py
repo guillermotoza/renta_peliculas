a2c625317a2d8c2eaefc095e73adb23b7bec00b2
guillermotoza <guillermotoza@gmail.com>
2024-12-31 08:30:02 -0600

    update membership

--- sistemardp/membership/views.py
+++ sistemardp/membership/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 from .models import Membership, UserMembership, Subscription
+from django.utils import timezone
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
@@ -27,9 +28,25 @@ def membership_detail(request, membership_slug):
 def subscribe(request, membership_slug):
     membership = Membership.objects.get(slug=membership_slug)
     user_membership, created = UserMembership.objects.get_or_create(user=request.user)
+    
+    # Verificar si el usuario ya tiene una suscripción activa
+    active_subscription = Subscription.objects.filter(
+        user_membership=user_membership,
+        active=True,
+        end_date__gt=timezone.now()
+    ).exists()
+
+    if active_subscription:
+        # Redirigir al usuario con un mensaje de error
+        return render(request, 'error.html', {
+            'message': 'Ya tienes una membresía activa. No puedes agregar otra hasta que expire.'
+        })
+
+    # Crear o actualizar la suscripción
     user_membership.membership = membership
     user_membership.save()
-    Subscription.objects.create(user_membership=user_membership, end_date='2024-12-31')
+    Subscription.objects.create(user_membership=user_membership, end_date=timezone.now() + timezone.timedelta(days=30))
+    
     return redirect('membership_list')
 
 @login_required
@@ -42,7 +59,7 @@ def create_checkout_session(request):
                 'product_data': {
                     'name': 'Membresía Gold',
                 },
-                'unit_amount': 2000,  # Precio en centavos
+                'unit_amount': 2000,  
             },
             'quantity': 1,
         }],
@@ -54,8 +71,8 @@ def create_checkout_session(request):
 
 @csrf_exempt
 def payment_success(request):
-    return render(request, 'membership/payment_success.html')
+    return render(request, 'payment_success.html')
 
 @csrf_exempt
 def payment_cancel(request):
-    return render(request, 'membership/payment_cancel.html')
+    return render(request, 'payment_cancel.html')
