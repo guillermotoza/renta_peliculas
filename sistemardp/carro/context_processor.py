dd56ae70afd8c0c3f9b16ed58a3f2768f0c02de5
moisesr99 <45242281+moisesr99@users.noreply.github.com>
2025-01-02 14:03:25 -0600

    Update context_processor.py

--- sistemardp/carro/context_processor.py
+++ sistemardp/carro/context_processor.py
@@ -1,18 +1,18 @@
 from decimal import Decimal
 
-def importe_total_carro(request):
-    # Verifica si 'carro' existe en la sesión, si no, usa un diccionario vacío
-    carro = request.session.get('carro', {})
-    total = sum(item['price'] * item['quantity'] for item in carro.values())
-    return {'importe_total_carro': total}
-
 #def importe_total_carro(request):
-#    total=0
- #   if request.user.is_authenticated: # hasta que existan una autentificacion de 
-  #      carro = request.session.get("carro", {})
-   #     for key, value in request.session["carro"].items():
-    #        if Decimal(value["descuento"]) > 0:
-     #           total+=(Decimal(value["precio_final"]))
-      #      else:
-       #         total+=(Decimal(value["precio"]))
-    #return {"importe_total_carro":total}    
+    # Verifica si 'carro' existe en la sesiÃ³n, si no, usa un diccionario vacÃ­o
+    #carro = request.session.get('carro', {})
+    #total = sum(item['price'] * item['quantity'] for item in carro.values())
+    #return {'importe_total_carro': total}
+
+def importe_total_carro(request):
+    total=0
+    if request.user.is_authenticated: # hasta que existan una autentificacion de 
+        carro = request.session.get("carro", {})
+        for key, value in request.session["carro"].items():
+            if Decimal(value["descuento"]) > 0:
+                total+=(Decimal(value["precio_final"]))
+            else:
+                total+=(Decimal(value["precio"]))
+    return {"importe_total_carro":total}    
