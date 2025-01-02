7088aa2f8c0909f9561dc35ec7b46aec84cc16fa
moisesr99 <45242281+moisesr99@users.noreply.github.com>
2025-01-02 13:38:53 -0600

    Update urls.py

--- sistemardp/pedidos/urls.py
+++ sistemardp/pedidos/urls.py
@@ -4,5 +4,6 @@ from pedidos import views
 
 urlpatterns = [
     path('', views.procesar_pedido,name="pedidos"),
-    
+    path('generar_pdf_pedido/<int:pedido_id>/', views.generar_pdf_pedido, name='generar_pdf_pedido'),
+    path('generar_excel_pedido/<int:pedido_id>/',views.generar_excel_pedido,name='generar_excel_pedido'),
 ]
